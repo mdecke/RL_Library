ff317ef71191a7aeb0b277588fa2ee464efdf7f5
mdecke <mdecke@student.ethz.ch>
2025-10-15 13:56:57 -0700

    take algo choice from cli args rather than writting it to config file

--- agents/__init__.py
+++ agents/__init__.py
@@ -1,3 +1,5 @@
+from typing import Dict
+
 from .DDPG_agent import DdpgAgent
 from .TD_agent import TdAgent
 # from .SAC_agent import SACAgent
@@ -8,8 +10,8 @@ AGENTS = {
     "tdn": TdAgent,
 }
 
-def create_agent(env, cfg):
-    agent_type = cfg["algorithm"].strip().lower()
+def create_agent(env, algo_type:str, cfg:Dict):
+    agent_type = algo_type.strip().lower()
     if agent_type not in AGENTS:
         raise ValueError(f"Agent type '{agent_type}' is not recognized. Available types: {list(AGENTS.keys())}")
     agent_class = AGENTS[agent_type]
