00b09bf9dafd0b0c30fc68984ee67e8811fde855
mdecke <mdecke@student.ethz.ch>
2025-10-17 15:06:53 -0700

    mse regularization for mean in nll

--- base_classes/utils.py
+++ base_classes/utils.py
@@ -4,6 +4,7 @@ from typing import Dict, Tuple
 import pandas as pd
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 import torch.distributions as dist
 from torchinfo import summary
 import yaml
@@ -87,11 +88,12 @@ def load_file(filepath: str) -> pd.DataFrame:
     else:
         raise ValueError(f"Unsupported file format for {filepath}. Supported formats are: .csv, .xlsx, .xls, .pkl, .pickle, .json")
     
-def gaussian_nll_loss(mu:torch.Tensor, log_sigma:torch.Tensor, target:torch.Tensor)->torch.Tensor:
+def gaussian_nll_loss(mu:torch.Tensor, log_sigma:torch.Tensor, target:torch.Tensor, regularization_weight:float=0.5)->torch.Tensor:
     sigma = torch.exp(log_sigma) + 1e-5  # Ensure sigma is not zero for numerical stability
     nll = 0.5 * torch.log(2 * torch.pi * (sigma ** 2)) + 0.5 * ((target - mu) ** 2) / (sigma ** 2) # also 0.5 * torch.log(2 * torch.pi) + 0.5 * log_sigma + 0.5 * ((target - mu) ** 2) / (sigma ** 2)  -- valid formulation
     # loss = nn.GaussianNLLLoss() might be pb mean over batch dimension
-    return nll.mean()
+    mse = F.mse_loss(mu, target) 
+    return nll.mean() + mse*regularization_weight
 
 def make_data_frame(data_dir:str) -> pd.DataFrame:
     csv_paths = []
