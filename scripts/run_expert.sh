aae1b95083b43f8678ef6fc71d59d0b65745aeb4
mdecke <mdecke@student.ethz.ch>
2025-10-16 17:08:59 -0700

    separate expert and rl runs

---
+++ scripts/run_expert.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+TASK="Pendulum-v1" # Options: Pendulum-v1, HalfCheetah-v4, Ant-v5, Hopper-v4
+ALGO="tdn" # Options: ddpg, tdn, sac
+
+
+
+echo "=== Expert Modeling ==="
+python scripts/expert_fitting.py \
+    --expert_type mle \
+    --expert_domain time \
+    --expert_data_path ./logs/$TASK/$ALGO/validation_stats \
+    --path_to_saved_expert ./saved/$TASK/expert
