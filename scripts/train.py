1ac9152a14abe1eb908741845c85f5562ae6c4f9
mdecke <mdecke@student.ethz.ch>
2025-11-04 17:35:42 -0800

    best avg return for saving policy

--- scripts/train.py
+++ scripts/train.py
@@ -139,7 +139,7 @@ def main():
                 })
             
             avg_return.append(torch.mean(cumulative_reward[env_idx,:].cpu()))
-            best_ending = torch.max(cumulative_reward[env_idx,:].cpu())
+            # best_ending = torch.max(cumulative_reward[env_idx,:].cpu())
             best_ending = avg_return[-1] 
             if best_ending >= BEST_SO_FAR:
                 BEST_SO_FAR = best_ending
